eb77683bffac5b705e1865256c5adaf3d58e5c86
Alex Reimann Cunha Lima <reimanncl@gmail.com>
2012-10-17 01:04:49 +0200

    Versao inicial

---
+++ index.php
@@ -0,0 +1,220 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+        "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <title>
+    Página pessoal institucional de Alex Reimann Cunha Lima
+    </title>
+    <style type="text/css">
+        body {
+            background: #ddddff;
+            margin: 0;
+            padding: 20px;
+            line-height: 1.4em;
+            font-family: tahoma, arial, sans-serif;
+            font-size: 100%;
+        }
+
+        table {
+            //margin-left: auto;
+            //margin-right: auto;
+            width: 80%;
+            //margin: 0;
+            background: #F5F5E5;
+            border: 1px solid #333333;
+            border-collapse: collapse;
+            margin: 16px auto 16px auto
+        }
+
+        td, th {
+            // border-bottom: 1px solid #333333;
+            border: 1px solid #333333;
+            padding: 1px 4px;
+        }
+
+        td {
+        //    border: 1px solid #333333;
+            text-align: left;
+        }
+
+        th {
+            background: #EEEEEE;
+            text-align: center;
+        }
+
+        caption {
+            background: #E0E0E0;
+            margin: 0;
+            border: 1px solid #333333;
+            border-bottom: none;
+            padding: 6px 16px;
+            font-weight: bold;
+        }
+    </style>
+</head>
+<body>
+<h1>Alex Reimann Cunha Lima</h1>
+<p>Meu nome é Alex Reimann Cunha Lima. Sou aluno do quarto período do curso de Ciência da Computação da Universidade Federal da Fronteira Sul.
+Para entrar em contato comigo, basta enviar-me um <a href="mailto:ihatespam@goodlucktryingthishost.com">e-mail</a>.</p>
+<table>
+    <caption>
+    Trabalhos desenvolvidos ao longo do curso
+    </caption>
+    <thead>
+        <tr>
+            <th>Semestre</th><th>Componente(*)</th><th>Trabalho</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td>primeiro</td>
+            <td>II</td>
+            <td>Máquina de Turing simulada na linguagem BASIC em ambiente MS-DOS.</td>
+        </tr>
+        <tr>
+            <td>primeiro</td>
+            <td>MI / AP</td>
+            <td>Desenho de gráficos no plano cartesiano a partir de funções fornecidas pelo usuário com uma máquina virtual própria.</td>
+        </tr>
+        <tr>
+            <td>primeiro</td>
+            <td>AP</td>
+            <td>Diversos programas em C com objetivo exclusivo de aprendizado, direcionados a fazer coisas extremamente simples, como
+                verificar a primalidade de números.</td>
+        </tr>
+        <tr>
+            <td>segundo</td>
+            <td>SD</td>
+            <td>Vídeo-game com microprocessador próprio em hardware programável (não chegou a funcionar fora da simulação porque
+                a universidade não fornceu aos alunos acesso ao kit).
+            </td>
+        </tr>
+        <tr>
+            <td>segundo</td>
+            <td>ED1</td>
+            <td>Diversos programas em C com objetivo exclusivo de aprendizado, direcionados a fazer coisas extremamente simples,
+                como criar e manipular listas encadeadas e pilhas.</td>
+        </tr>
+        <tr>
+            <td>segundo</td>
+            <td>EB</td>
+            <td>Programa em C destinado a extrair informações estatísticas de um conjunto de dados informado pelo usuário.</td>
+        </tr>
+        <tr>
+            <td>terceiro</td>
+            <td>P1</td>
+            <td>Criação de linguagem de programação própria (Palíndromo), com máquina virtual e "interpretador" programados em Java.
+                Até onde se tem notícia, é a única linguagem de programação no mundo com laços de execução bidirecionais (na verdade
+                ela toda é bidirecional).</td>
+        </tr>
+        <tr>
+            <td>terceiro</td>
+            <td>ED2</td>
+            <td>Diversos programas em C com objetivo exclusivo de aprendizado, direcionados a criar árvores AVL, Preto-vermelhas, B e ainda
+                um outro destinado a realizar ordenação externa em fitas simuladas por arquivos.</td>
+        </tr>
+        <tr>
+            <td>terceiro</td>
+            <td>P1</td>
+            <td>Escalonador de matrizes com um jogo no estilo Rogue incluído, desenvolvido em Java para Android.</td>
+        </tr>
+        <tr>
+            <td>terceiro</td>
+            <td>P1</td>
+            <td>Codificação da inteligência artificial de agentes trabalhando em equipe para derrotar os adversários em uma arena, feito em Java.
+                Na realidade não havia muita inteligência nos agentes, pois eles apenas se dispunham sobre uma das diagonais da arena e lá ficavam
+                parados aguardando a chegada dos cogumelos.</td>
+        </tr>
+        <tr>
+            <td>terceiro</td>
+            <td>OC</td>
+            <td>Jogo da velha programado em assembly para MIPS.</td>
+        </tr>
+        <tr>
+            <td>terceiro</td>
+            <td>OC</td>
+            <td>Simulador de memória cache programado em C.</td>
+        </tr>
+    </tbody>    
+</table>
+
+<table>
+    <caption>
+    Interesses de aprendizado tecnológico ao longo do curso
+    </caption>
+    <thead>
+        <tr>
+            <th>Prioridade</th><th>Nome da tecnologia</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td>1</td><td>Linguagem de programação Haskell</td>
+        </tr>
+        <tr>
+            <td>2</td><td>Linguagem de programação Lisp - dialeto Scheme</td>
+        </tr>
+        <tr>
+            <td>3</td><td>Linguagem de programação Lisp - dialeto Common Lisp</td>
+        </tr>
+        <tr>
+            <td>4</td><td>Linguagem de programação EcmaScript</td>
+        </tr>
+        <tr>
+            <td>5</td><td>Linguagem de estilos CSS</td>
+        </tr>
+        <tr>
+            <td>6</td><td>Banco de dados MySQL</td>
+        </tr>
+        <tr>
+            <td>7</td><td>Sistema de controle de versão Git</td>
+        </tr>
+        <tr>
+            <td>8</td><td>Sistema de controle de versão Mercury</td>
+        </tr>
+        <tr>
+            <td>9</td><td>Linguagem de marcação HTML</td>
+        </tr>
+    </tbody>
+</table>
+
+<table>
+    <caption>
+    (*) Legenda dos componentes curriculares
+    </caption>
+    <thead>
+        <tr>
+            <th>Código</th><th>Nome do Componente</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td>II</td><td>Introdução à Informática</td>
+        </tr>
+        <tr>
+            <td>AP</td><td>Algoritmos e Programação</td>
+        </tr>
+        <tr>
+            <td>MI</td><td>Matemática Instrumental</td>
+        </tr>
+        <tr>
+            <td>ED1</td><td>Estrutura de Dados I</td>
+        </tr>
+        <tr>
+            <td>ED2</td><td>Estrutura de Dados II</td>
+        </tr>
+        <tr>
+            <td>SD</td><td>Sistemas Digitais</td>
+        </tr>
+        <tr>
+            <td>P1</td><td>Programação I</td>
+        </tr>
+        <tr>
+            <td>OC</td><td>Organização de Computadores</td>
+        </tr>
+    </tbody>
+</table>
+
+</body>
+</html>
